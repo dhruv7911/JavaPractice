e6ad4db1f559e98969e1b201c593bb49526e1bed
dhruv kumar yadav <4444dhruv@gmail.com>
2025-03-25 15:01:10 +0530

    Inheritance define class

---
+++ Inheritance/Employee.java
@@ -0,0 +1,11 @@
+public class Employee{//super class base class
+    private String name;
+    private String id;
+
+    public static void main(String[] args){
+		    
+    }
+}
+class Manager Extends Employee{//subclass extends Employee class
+	//added method and fields
+}
