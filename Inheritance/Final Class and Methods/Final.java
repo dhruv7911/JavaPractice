010223d26a4bb83107c7d50382479b9bf3b8cb8d
Dhruv Kumar Yadav <4444dhruv@gmail.com>
2025-03-29 18:23:54 +0530

    final class

---
+++ Inheritance/Final Class and Methods/Final.java
@@ -0,0 +1,14 @@
+class Employee{
+
+}
+class Manager extends Employee{
+
+}
+final class Executive extends Manager{
+
+}
+class Final extends Executive{
+    public static void main(String[] args){
+
+    }
+}
