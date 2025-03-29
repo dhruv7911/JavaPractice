2a2ef3c25760a95b4b7424d294530cd6147dd15e
Dhruv Kumar Yadav <4444dhruv@gmail.com>
2025-03-29 20:52:11 +0530

    incomplete abstract

---
+++ Abstract Class/Example1/Index.java
@@ -0,0 +1,9 @@
+
+abstract class Vehicle{
+    private String make;
+    private String model;
+    private int year;
+    public abstract void calculate_fuel_efficiency();
+    public abstract void calculate_travel_distanc();
+
+}
