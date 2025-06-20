d8b944ee43ed67da202c4d3a2c8328d43d1bb051
blogger1233 <4444dhruv@gmail.com>
2025-06-20 18:10:07 +0530

    Floating-point doubl

---
+++ primitive data types/Floating-point/Index.java
@@ -0,0 +1,12 @@
+public class Index{
+    public static void main(String[] args){
+	double pi,r,a;
+
+	r = 10.8;// radius of circle
+	pi = 3.1416;// approx value of pi
+	
+	a = pi*r*r; // compute area of circle
+	System.out.println("Area of circle(r: "+r+")="+a);
+
+    }
+}
