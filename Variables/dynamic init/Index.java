03fb25f56d0b114062e368ed78f88b27e6194c68
blogger1233 <4444dhruv@gmail.com>
2025-06-21 10:23:28 +0530

    Variables

---
+++ Variables/dynamic init/Index.java
@@ -0,0 +1,9 @@
+//Demonstrate Dynamic initalization
+public class Index{
+    public static void main(String[] args){
+	double a=3.0,b=4.0;
+	//c is dynamically initalized
+	double c = Math.sqrt(a*a+b*b);
+	System.out.println("Hypotenus: "+c);
+    }    
+}
