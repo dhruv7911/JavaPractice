dc81eb10a93deeff1e888f160bf59482d38d282b
blogger1233 <4444dhruv@gmail.com>
2025-06-28 14:48:52 +0530

    simple example

---
+++ automatic type conversion/Index2.java
@@ -0,0 +1,8 @@
+public class Index2{
+    public static void main(String[] args){
+	byte b = 10;
+	b = b*2;//Automatic type conversion lead to error
+	//Index2.java:4: error: incompatible types: possible lossy conversion f
+`
+    }
+}
